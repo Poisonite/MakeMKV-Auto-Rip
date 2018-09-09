5af04203bf16f93451bf689d1b7225f64c9705e4
Poisonite101 <zacace@ingoglia.com>
2018-09-09 02:15:11 -0700

    Dev Patch 2
    -Fixed DriveLoader.bat Again

--- DriveLoader.bat
+++ DriveLoader.bat
@@ -59,6 +59,7 @@ echo ---Loading All DVDs---
 ping 192.0.2.2 -n 1 -w 400 > nul
 node Load.js
 pause
+:eof
 
 ::Copy Movies (For Option 2)
 :EjectDVDs
@@ -67,3 +68,6 @@ echo ---Ejecting All DVDs---
 ping 192.0.2.2 -n 1 -w 400 > nul
 node Eject.js
 pause
+:eof
+
+:eof
